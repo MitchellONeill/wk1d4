c0fb85fb997b64d5f236a568029d7303eba4168f
Mitchell ONeillgit config --global user.name Mitchell <7moneill@gmail.com>
2016-09-08 20:57:34 +0000

    combined both sorting assignments into one js file.

--- sort.js
+++ sort.js
@@ -5,3 +5,20 @@ sorted = numList.sort(function(a, b){
 })
 
 console.log(sorted)
+
+////////
+
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+studSort = students.sort(function(a, b){
+  if(a.name === b.name) {
+    return b.age - a.age;
+  }
+    return a.name > b.name;
+  });
+console.log(studSort);
